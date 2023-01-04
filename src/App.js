cad4bec9529c11d9a257b553e6136dda463d0406
mayeskhatri243 <mayeskhatri243@gmail.com>
2023-01-04 15:58:22 +0530

    some changes in mobile responsive

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <div className="sticky top-0 z-10 flex items-center border-b border-white/20 bg-gray-800 pl-1 pt-1 text-gray-200 sm:pl-3 md:hidden">
         <button
           type="button"
-          className={`-ml-0.5 -mt-0.5 inline-flex h-10 w-10 items-center justify-center rounded-md focus:ring-1 focus:ring-white ${
+          className={`-ml-0.5 -mt-0.5 inline-flex h-10 w-10 items-center outline-none justify-center rounded-md focus:ring-1 focus:ring-white ${
             !show && "!ring-0"
           } dark:hover:text-white text-gray-100`}
           onClick={() => setShow(!show)}
